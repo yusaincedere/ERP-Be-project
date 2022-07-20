46f132598a7c5229c38f8f05f03db7654637a267
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-20 15:59:57 +0300

    stock card model created

---
+++ src/main/java/com/iknow/stocktrackingbe/model/stock/StockCard.java
@@ -0,0 +1,38 @@
+package com.iknow.stocktrackingbe.model.stock;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+public class StockCard implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String stockCode;
+
+    private String name;
+
+    private Long stockCount;
+    private Long safeStockCount;
+    private Long min;
+    private Long max;
+
+    private LocalDate expectedSupplyDate;
+
+
+}
