7cc942dd0c0887e1853f542e851d0e1962d8b153
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-20 15:59:38 +0300

    product model created

---
+++ src/main/java/com/iknow/stocktrackingbe/model/product/Product.java
@@ -0,0 +1,59 @@
+package com.iknow.stocktrackingbe.model.product;
+
+import com.iknow.stocktrackingbe.model.Prospectus;
+import com.iknow.stocktrackingbe.model.stock.StockCard;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productCode;
+
+    private String productName;
+
+    private Double productMilligramWeight;
+
+    private LocalDate expiryDate;
+
+    private LocalDate produceDate;
+
+    private BigDecimal price;
+
+    @Enumerated(EnumType.STRING)
+    private CurrencyType currencyType;
+
+    private String amountOfUsage;
+
+    @OneToOne
+    private Prospectus prospectus;
+
+    @OneToOne
+    private StockCard stockCard;
+
+    @NotNull
+    private Long safetStockCount;
+
+    @ManyToMany(cascade = CascadeType.PERSIST)
+    private List<ProductIngredient> productIngredients;
+
+
+}
