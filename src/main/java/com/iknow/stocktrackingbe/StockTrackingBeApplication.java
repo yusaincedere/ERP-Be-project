f3064ac1cc1245ba730ddfde75532a32971ec514
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-20 13:43:45 +0300

    project created

---
+++ src/main/java/com/iknow/stocktrackingbe/StockTrackingBeApplication.java
@@ -0,0 +1,13 @@
+package com.iknow.stocktrackingbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTrackingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTrackingBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iknow/stocktrackingbe/StockTrackingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iknow.stocktrackingbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTrackingBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
